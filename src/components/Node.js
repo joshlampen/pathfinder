f21781b9927f097811043eb31dd52a02e1cff93b
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-02 18:05:58 -0400

    Refined the grid to include nodes

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import '../styles/Node.css';
 
-export default function Node() {
+export default function Node(props) {
+    const { isStart, isFinish } = props;
+
+    const extraClassName = isStart ? 'node-start' : isFinish ? 'node-finish' : '';
+
     return (
-        <div className="Node"></div>
+        <div className={`Node, ${extraClassName}`}></div>
     )
 }
