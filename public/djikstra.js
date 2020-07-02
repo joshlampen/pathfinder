42e342460b792c66ae8d433251f7da2e3a15c788
Aaron <timbolaj@gmail.com>
2020-07-02 18:59:51 -0400

    Pseudocode for Dijkstra's algorithm is written

---
+++ public/djikstra.js
@@ -0,0 +1,8 @@
+let graph = {
+	start: { A: 5, B: 2 },
+	A: { start: 1, C: 4, D: 2 },
+	B: { A: 8, D: 7 },
+	C: { D: 6, finish: 3 },
+	D: { finish: 1 },
+	finish: {},
+};
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,6 +2,30 @@ import React, { useState } from 'react';
 import Node from './Node';
 import '../styles/Grid.css';
 
+// set grid equal to getInitialGrid() which is similar to what we have below but pushes nodes using createNode()
+// createNode() returns objects with keys such as col, row, isStart, isFinish, distance, etc.
+// within dijkstra:
+//  initialize an empty array that contains visitedNodesInOrder
+//  set startNode.distance = 0
+//  set unvisitedNodes = grid array of all the nodes
+//  while there are still nodes in the unvisitedNodes array...
+//    sort the nodes in the array by distance
+//    set closestNode = unvisitedNodes.shift() --> assigns next node in array while also removing from array for future
+//    if closestNode.isWall then skip it (it's a wall)
+//    if closestNode.distance === Infinity we're trapped and should stop
+//    set closestNode.isVisited = true
+//    push closestNode into the visitedNodesInOrder array
+//    if closestNode === finishNode you're done --> return visitedNodesInOrder
+//    at the end, upDateUnvisitedNeigbors
+//      sets unvisitedNeigbors equal to getUnvisitedNeighbors
+//        creates an empty neighbors array
+//        based on the position of the node being passed into the function, the function pushes all adjacent nodes into the array
+//        returns all items of the array, filtering by ifVisited is true
+//      for each neighbor of the array...
+//        change distance from Infinity to original node distance + 1
+//        set previousNode to the original node
+
+
 export default function Grid() {
   // const [nodes, setNodes] = useState([])
 
