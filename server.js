3bcb4b872fb606dff3d8b22b47e5723cc6ee0b02
JoshLampen <joshlampen2@gmail.com>
2020-07-07 18:50:52 -0400

    Add cors module

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors())
 
 app.use(express.static(path.resolve(__dirname, 'build')));
 
