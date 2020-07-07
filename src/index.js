53c2314f3c72a6670f0afebf771a38e0c22dcbf5
JoshLampen <joshlampen2@gmail.com>
2020-07-07 17:56:51 -0400

    Enable axios base URL change for heroku

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import axios from "axios";
 // import * as serviceWorker from '/serviceWorker';
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 ReactDOM.render(
   <React.StrictMode>
