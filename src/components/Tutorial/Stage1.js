c9135e8c36e12fe1063688d4aab0b63a00c09bc4
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-09 18:42:17 -0400

    Added tutorial stage 1 content

--- src/components/Tutorial/Stage1.js
+++ src/components/Tutorial/Stage1.js
@@ -12,9 +12,18 @@ export default function Stage1() {
 
     <div>
       <h3>Choosing Algorithms</h3>
-      <div>This is a test</div>
-      <span>In order to select an algorithm to render on the page, click the following toggle button on the upper right hand corner the screen</span>
-      <img></img>      
+      <span className={"textBody"}>Pathfinder uses a number of different algorithms to visualize on the grid. By default, the app renders <strong> Dijkstra’s Algorithm </strong> once loaded.
+</span>
+      <br></br>
+      <span className={"textBody"}>In order to select an algorithm to render on the page, click the following toggle button on the upper right hand corner the screen
+</span>
+<br></br>
+      <img id='menu' src='images/stage1TutorialMenu.png'></img> 
+      <br></br> 
+      <span className={"textBody"}>Once selected, you should notice a change on the algorithm’s description above the grid. 
+</span>
+<br></br>
+<img id='desc' src='images/stage1TutorialDesc.png'></img> 
     </div>
 
   )
