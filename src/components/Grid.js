0b90c158638dce589358aeaa7b36f8f86653a250
JoshLampen <joshlampen2@gmail.com>
2020-07-10 09:55:24 -0400

    Refactor code for generating maze

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import Node from "./Node";
 import BasicButton from "./BasicButton";
@@ -31,41 +31,12 @@ export default function Grid(props) {
 
   const {
     mazeWalls,
+    generateMazeWalls,
     generateMaze
   } = useMaze();
 
   useEffect(() => {
-    const maze = state.grid.map(row => {
-      return row.map(node => {
-        const newNode = { ...node }
-
-        if (mazeWalls.includes(node)) newNode.isWall = true;
-
-        return newNode
-      })
-    })
-
-    for (let row = 1; row <= maze.length - 2; row++) { // guarantee four spaces in each row
-      const spaces = [Math.ceil(Math.random() * 11), Math.ceil(11 + (Math.random() * 11)), Math.ceil(22 + (Math.random() * 11)), Math.ceil(33 + (Math.random() * 10))];
-      // const spaces = [Math.ceil(Math.random() * 14), Math.ceil(14 + (Math.random() * 14)), Math.ceil(28 + (Math.random() * 15))];
-  
-      for (const space of spaces) {
-        maze[row][space].isWall = false;
-      }
-    }
-
-    for (let col = 1; col <= maze[0].length - 2; col++) { // guarantee two spaces in each column
-      const spaces = [Math.ceil(Math.random() * 7), Math.ceil(7 + (Math.random() * 6))];
-
-      for (const row of maze) {
-        for (const space of spaces) {
-          if (maze.indexOf(row) === space) {
-            maze[space][col].isWall = false;
-          }
-        }
-      }
-    }
-
+    const maze = generateMaze(state.grid, mazeWalls)
     loadWalls2(maze)
   }, [mazeWalls])
 
@@ -121,7 +92,7 @@ export default function Grid(props) {
             text='Generate Maze'
             size='small'
             color='secondary'
-            onClick={() => generateMaze(state.grid, 0, 14, 0, 44, 'horizontal', null)}
+            onClick={() => generateMazeWalls(state.grid, 0, 14, 0, 44, 'horizontal', null)}
             inProgress={state.inProgress}
           />
           <ToolBarDropdown
